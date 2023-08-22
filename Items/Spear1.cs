22228b93d8991edeba7855c5ab745736eeadce02
SpongesLost <thespongeisop@gmail.com>
2023-08-22 15:10:05 +0200

    Update 1
    Contains the beginnings of the new spear

---
+++ Items/Spear1.cs
@@ -0,0 +1,37 @@
+﻿using advmod.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace advmod.Items
+{
+	public class Spear1 : ModItem
+	{
+		public override void SetDefaults() {
+			Item.damage = 10;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.useAnimation = 18;
+			Item.useTime = 24;
+			Item.shootSpeed = 3.7f;
+			Item.knockBack = 6.5f;
+			Item.width = 32;
+			Item.height = 32;
+			Item.scale = 1f;
+			Item.rare = ItemRarityID.Pink;
+			Item.value = Item.sellPrice(silver: 10);
+
+			Item.DamageType = DamageClass.MeleeNoSpeed/* tModPorter Suggestion: Consider MeleeNoSpeed for no attack speed scaling */;
+			Item.noMelee = true; // Important because the spear is actually a projectile instead of an item. This prevents the melee hitbox of this item.
+			Item.noUseGraphic = true; // Important, it's kind of wired if people see two spears at one time. This prevents the melee animation of this item.
+			Item.autoReuse = true; // Most spears don't autoReuse, but it's possible when used in conjunction with CanUseItem()
+
+			Item.UseSound = SoundID.Item1;
+			Item.shoot = ModContent.ProjectileType<Spear1Projectile>();
+		}
+
+		public override bool CanUseItem(Player player) {
+			// Ensures no more than one spear can be thrown out, use this when using autoReuse
+			return player.ownedProjectileCounts[Item.shoot] < 1;
+		}
+	}
+}
